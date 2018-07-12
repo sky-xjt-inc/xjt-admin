0b5e46b302057ff4cd3fd9fc19a9ba0597a4489c
Hilkiah Makemo <hilmak01@gmail.com>
2018-07-12 08:30:55 -0600

    Initial push from local

---
+++ code/LeftAndMainExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+use SilverStripe\Admin\LeftAndMainExtension;
+
+class CMSBrandingLeftAndMainExtension extends LeftAndMainExtension {
+
+    public function init(){
+        $config = SiteConfig::current_site_config();
+
+        if($config->CustomApplicationLink) {
+            Config::inst()->update('LeftAndMain', 'application_link', $config->CustomApplicationLink);
+        }
+
+        if($config->CustomApplicationName) {
+            Config::inst()->update('LeftAndMain', 'application_name', $config->CustomApplicationName);
+        }
+
+        if(!$config->ShowReportAdmin) {
+            CMSMenu::remove_menu_item('ReportAdmin');
+        }
+
+        if(!$config->ShowHelpLink) {
+            CMSMenu::remove_menu_item('Help');
+        }
+
+    }
+
+}
